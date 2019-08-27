212886ee50a9bbdd433e20b8049e515fa41bf885
Kboy <yankang_hpu@163.com>
2019-08-27 15:12:51 +0800

    初始化业务A项目

---
+++ A_Section/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  A_Section
+//
+//  Created by yankang on 2019/8/27.
+//  Copyright © 2019 yankang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ A_Section/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  A_Section
+//
+//  Created by yankang on 2019/8/27.
+//  Copyright © 2019 yankang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
